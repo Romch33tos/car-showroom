f731d0538157beaa93f5967ccc893b6200802129
Roman Khadraliev <Romch33tos@yandex.ru>
2025-05-21 19:18:48 +0700

    Интерфейс представления формы заказа

---
+++ IOrderView.cs
@@ -0,0 +1,14 @@
+﻿namespace CarShowroom
+{
+  public interface IOrderView
+  {
+    Car SelectedCar { get; }
+    string CustomerName { get; }
+    string CustomerPhone { get; }
+    string CustomerEmail { get; }
+
+    event EventHandler PlaceOrderClicked;
+    void ShowMessage(string message);
+    void CloseForm();
+  }
+}
